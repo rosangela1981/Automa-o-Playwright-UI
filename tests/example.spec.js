e0c13019152b3fa9535bff4a1607f3815e764a4c
rosangela1920 <rosangelanascimentotecnol@hotmail.com>
2026-05-19 14:24:18 -0300

    alterando meus arquivos

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -54,24 +54,6 @@ test('Login com sucesso 1 @login', async ({ page }) => {
 });
 
 
-test('Login com sucesso 2 @login', async ({ page }) => {
-  const texto = await page.waitForSelector('text=NEWSLETTER');
-  await texto.scrollIntoViewIfNeeded()
-  
-  await page.getByRole('button', { name: 'Send Mail' }).click();
-  await page.getByRole('link', { name: ' Login' }).click();
-  await page.locator('#user').click();
-  await page.locator('#user').fill('rosangelanascimentotecnol@gmail.com');
-    await page.screenshot({ path: 'screenshot1.png', fullPage: true });
-  await page.locator('#password').click();
-  await page.locator('#password').fill('123456');
-  await page.locator('#password').screenshot({ path: 'screenshot/test1_elemento_senha.png' });
-  await page.screenshot({ path: 'screenshot2.png', fullPage: true });
-  await page.getByRole('button', { name: 'login' }).click();
-  await expect(page.getByText('Login realizado')).toBeVisible();
-  await page.screenshot({ path: 'screenshot3.png', fullPage: true });
- 
-});
 
 test.afterEach(async ({ page }) => {
   await page.close();
